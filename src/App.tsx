649c3c0d4cc274c3282ee58eb4e598549c670d9b
jeevanthask <jeevanthanandika@gmail.com>
2024-04-15 21:46:40 +0530

    dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import "primereact/resources/themes/saga-purple/theme.css";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-12">
-          <h1>Home Page</h1>
+    <>
+      <nav className="navbar bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+
+      <div className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-md-12">
+            <Dashboard />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,44 @@
+import { Card } from "primereact/card";
+
+function Dashboard() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="row">
+            <div className="col-md-4">
+              <Card title="Open">
+                <p className="m-0">34</p>
+              </Card>
+            </div>
+            <div className="col-md-4">
+              <Card title="In-Progress">
+                <p className="m-0">12</p>
+              </Card>
+            </div>
+            <div className="col-md-4">
+              <Card title="Closed">
+                <p className="m-0">2</p>
+              </Card>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <Card title="Latest Issues">
+            <p>Broken Checkout Button</p>
+            <span className="badge text-bg-primary">In Progress</span>
+            <hr />
+            <p>Payment Refund Request</p>
+            <span className="badge text-bg-danger">Open</span>
+            <hr />
+            <p>Missing User Profile Data</p>
+            <span className="badge text-bg-success">Closed</span>
+            <hr />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
