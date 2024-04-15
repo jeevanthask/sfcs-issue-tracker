e3c2536afdebb6e730cea37a77d03b2d4075eef4
jeevanthask <jeevanthanandika@gmail.com>
2024-04-15 20:35:58 +0530

    initialcommit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Home Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
